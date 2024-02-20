14f9ccf7cb8855cd7e2a83e115ace0bf610381b7
ArtemWorkACC <asi@aiweapps.com>
2024-02-20 14:30:15 +0300

    Some new features

--- app/src/main/java/com/aiweapps/main_view_bank_app/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/aiweapps/main_view_bank_app/presentation/screens/main/MainScreen.kt
@@ -1,8 +1,10 @@
 package com.aiweapps.main_view_bank_app.presentation.screens.main
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,7 +12,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -33,7 +39,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,35 +53,11 @@ import com.aiweapps.main_view_bank_app.R
 class MainScreen: Screen {
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
     @Preview
     @Composable
     override fun Content() {
         val scrollState = rememberScrollState()
         Scaffold(
-            topBar = {
-                TopAppBar(
-                    modifier = Modifier.fillMaxWidth(),
-                    title = { Text(text = "My Toolbar") },
-                    navigationIcon = {
-
-                    },
-                    actions = {
-                        IconButton(onClick = { }) {
-                            Icon(
-                                imageVector = Icons.Default.Settings,
-                                contentDescription = null
-                            )
-                        }
-                        IconButton(onClick = { }) {
-                            Icon(
-                                imageVector = Icons.Default.Settings,
-                                contentDescription = null
-                            )
-                        }
-                    }
-                )
-            },
             bottomBar = {
             },
             content = {
@@ -90,6 +75,30 @@ class MainScreen: Screen {
                 .padding(vertical = 75.dp)
                 .background(Color(red = 0x12, green = 0x11, blue = 0x11))
         ) {
+            Row(modifier = Modifier.padding(all = 12.dp)) {
+                Image(
+                    painter = painterResource(R.drawable.ic_profile_picture),
+                    contentDescription = "profile picture",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                )
+
+                // Add a horizontal space between the image and the column
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column {
+                    Text(
+                        text = "Alex",
+                        color = Color(red = 0xFF, green = 0xFF, blue = 0xFF)
+                    )
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Text(
+                        text = "Good Morning",
+                        color = Color(red = 0xFF, green = 0xFF, blue = 0xFF)
+                    )
+                }
+            }
             // SearchBar
             var searchText by remember { mutableStateOf("") }
             TextField(
@@ -108,7 +117,9 @@ class MainScreen: Screen {
             Spacer(modifier = Modifier.height(16.dp))
 
             Row(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
@@ -116,21 +127,58 @@ class MainScreen: Screen {
                     fontSize = 19.sp,
                     color = Color(red = 0xFF, green = 0xFF, blue = 0xFF)
                 )
-                Text(
-                    text = "$154,06",
-                    modifier = Modifier,
-                    color = Color(red = 0xFF, green = 0xFF, blue = 0xFF)
-                )
+                Box(
+                    modifier = Modifier
+                        .background(
+                            Color(red = 0x28, green = 0x27, blue = 0x27),
+                            shape = RoundedCornerShape(6.dp)
+                        )
+                        .padding(horizontal = 4.dp, vertical = 4.dp)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Row() {
+                        Image(
+                            painter = painterResource(R.drawable.ic_cashback),
+                            contentDescription = "cashback picture",
+                            modifier = Modifier
+                                .size(20.dp)
+                        )
+                        Text(
+                            text = "$154,06",
+                            modifier = Modifier,
+                            color = Color(red = 0xFF, green = 0xFF, blue = 0xFF)
+                        )
+                    }
+                }
             }
-            Row() {
+            Spacer(modifier = Modifier.height(12.dp))
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            ) {
                 Text(
                     text = "$3.945,94",
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold,
                     color = Color(red = 0xFF, green = 0xFF, blue = 0xFF)
                 )
-                Text(
-                    text = "usd",
-                    color = Color(red = 0xFF, green = 0xFF, blue = 0xFF)
-                )
+                Spacer(modifier = Modifier.width(12.dp))
+                Box(
+                    modifier = Modifier
+                        .background(
+                            Color(red = 0xD3, green = 0xFF, blue = 0x52),
+                            shape = RoundedCornerShape(6.dp)
+                        )
+                        .padding(horizontal = 4.dp)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Text(
+                        text = "usd",
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(red = 0x00, green = 0x00, blue = 0x00)
+                    )
+                }
             }
             Text(
                 text = "My cards",
